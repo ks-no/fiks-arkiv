070c369191e2aa36318f43a69bac863e686774e4
Tor Kjetil Nilsen <tor@arkitektum.no>
2020-09-04 08:59:57 +0200

    first sok sample

---
+++ dotnet-source/ks.fiks.io.arkivintegrasjon.client/schema/genSokSchemaClasses.bat
@@ -0,0 +1,4 @@
+echo "sok.xsd"
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\xsd.exe" sok.xsd /c  /n:no.ks.fiks.io.arkivmelding.sok
+
+pause
---
+++ dotnet-source/ks.fiks.io.arkivintegrasjon.tests/UnitTestsSok.cs
@@ -0,0 +1,33 @@
+﻿using ks.fiks.io.fagsystem.arkiv.sample.ForenkletArkivering;
+using no.ks.fiks.io.arkivmelding.sok;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace ks.fiks.io.arkivintegrasjon.tests
+{
+    public class UnitTestsSok
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestSok()
+        {
+
+
+
+            var arkivmeldingsok = new sok();
+            arkivmeldingsok.respons = respons_type.mappe;
+            arkivmeldingsok.meldingId = Guid.NewGuid().ToString();
+            arkivmeldingsok.system = "Fagsystem X";
+            //TODO lage list på parameter i steden for []
+
+            string payload = Arkivintegrasjon.Serialize(arkivmeldingsok);
+
+            Assert.Pass();
+        }
+    }
+}
