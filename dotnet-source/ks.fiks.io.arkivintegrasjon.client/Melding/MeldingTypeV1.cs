996a72ad8033d24a72dc7aff84cad3a774d4e529
jarle børsheim <jarle.borsheim@gmail.com>
2021-06-14 20:10:29 +0200

    Flyttet typer og sjekk til egen klasse

--- dotnet-source/ks.fiks.io.arkivintegrasjon.client/Melding/MeldingTypeV1.cs
+++ dotnet-source/ks.fiks.io.arkivintegrasjon.client/Melding/MeldingTypeV1.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace ks.fiks.io.arkivintegrasjon.client.Melding
 {
     public static class MeldingTypeV1
@@ -8,5 +10,53 @@ namespace ks.fiks.io.arkivintegrasjon.client.Melding
 
         // Arkivintegrasjon
         public const string Mottatt = "no.ks.fiks.gi.arkivintegrasjon.mottatt.v1";
+        
+        // Basis
+        public const string BasisArkivmelding = "no.ks.fiks.gi.arkivintegrasjon.oppdatering.basis.arkivmelding.v1";
+        public const string BasisArkivmeldingUtgaaende = "no.ks.fiks.gi.arkivintegrasjon.oppdatering.basis.arkivmeldingUtgaaende.v1";
+        public const string ForenkletArkivmeldingInnkommende = "no.ks.fiks.gi.arkivintegrasjon.oppdatering.forenklet.arkivmeldingInnkommende.v1";
+        public const string BasisOppdaterSaksmappe = "no.ks.fiks.gi.arkivintegrasjon.oppdatering.basis.oppdatersaksmappe.v1";
+        
+        // Sok
+        public const string InnsynSok = "no.ks.fiks.gi.arkivintegrasjon.innsyn.sok.v1";
+        
+        // Avansert
+        public const string OppdateringArkivmelding = "no.ks.fiks.gi.arkivintegrasjon.oppdatering.arkivmelding.v1";
+        public const string OppdateringArkivmeldingUtgaaende =
+            "no.ks.fiks.gi.arkivintegrasjon.oppdatering.arkivmeldingUtgaaende.v1";
+        
+        public static readonly List<string> Basis = new List<string>()
+        {
+            BasisArkivmelding,
+            BasisArkivmeldingUtgaaende,
+            ForenkletArkivmeldingInnkommende,
+            BasisOppdaterSaksmappe
+        };
+            
+        public static readonly List<string> Sok = new List<string>()
+        {
+            InnsynSok
+        };
+
+        public static readonly List<string> Avansert = new List<string>()
+        {
+            OppdateringArkivmelding,
+            OppdateringArkivmeldingUtgaaende
+        };
+
+        public static bool IsBasis(string meldingsType)
+        {
+            return Basis.Contains(meldingsType);
+        }
+
+        public static bool IsSok(string meldingsType)
+        {
+            return Sok.Contains(meldingsType);
+        }
+        
+        public static bool IsAvansert(string meldingsType)
+        { 
+            return Avansert.Contains(meldingsType);
+        }
     }
 }
--- dotnet-source/ks.fiks.io.arkivsystem.sample/ArkivService.cs
+++ dotnet-source/ks.fiks.io.arkivsystem.sample/ArkivService.cs
@@ -10,15 +10,13 @@ using KS.Fiks.ASiC_E;
 using ks.fiks.io.arkivintegrasjon.client.ForenkletArkivering;
 using ks.fiks.io.arkivintegrasjon.client.Melding;
 using ks.fiks.io.arkivintegrasjon.common.AppSettings;
+using ks.fiks.io.arkivintegrasjon.common.FiksIOClient;
 using ks.fiks.io.arkivintegrasjon.sample.messages;
 using KS.Fiks.IO.Client;
-using KS.Fiks.IO.Client.Configuration;
 using KS.Fiks.IO.Client.Models;
 using ks.fiks.io.fagsystem.arkiv.sample.ForenkletArkivering;
-using Ks.Fiks.Maskinporten.Client;
 using Microsoft.Extensions.Hosting;
 using no.ks.fiks.io.arkivmelding;
-using RabbitMQ.Client;
 
 namespace ks.fiks.io.arkivsystem.sample
 {
@@ -30,7 +28,7 @@ namespace ks.fiks.io.arkivsystem.sample
         public ArkivService(AppSettings appSettings)
         {
             this.appSettings = appSettings;
-            client = CreateFiksIoClient();
+            client = FiksIOClientBuilder.CreateFiksIoClient(appSettings); //CreateFiksIoClient();
         }
         
         public void Dispose()
@@ -51,48 +49,20 @@ namespace ks.fiks.io.arkivsystem.sample
             //Se oversikt over meldingstyper på https://github.com/ks-no/fiks-io-meldingstype-katalog/tree/test/schema
 
             // Process the message
-            List<string> kjenteMeldingerBasis = new List<string>()
-                {
-                    "no.ks.fiks.gi.arkivintegrasjon.oppdatering.basis.arkivmelding.v1",
-                    "no.ks.fiks.gi.arkivintegrasjon.oppdatering.basis.arkivmeldingUtgaaende.v1",
-                    "no.ks.fiks.gi.arkivintegrasjon.oppdatering.forenklet.arkivmeldingInnkommende.v1",
-                    "no.ks.fiks.gi.arkivintegrasjon.oppdatering.basis.oppdatersaksmappe.v1"
-
-                };
-            
-            List<string> kjenteMeldingerSok = new List<string>()
-                {
-                    "no.ks.fiks.gi.arkivintegrasjon.innsyn.sok.v1",
-                    "no.ks.fiks.gi.arkivintegrasjon.innsyn.sok.v1"
-                };
-
-            List<string> kjenteMeldingerFiks = new List<string>()
-                {
-                    "no.ks.fiks.kvittering.tidsavbrudd",
-                    "no.ks.fiks.gi.arkivintegrasjon.feil.v1"
-                };
-
-            List<string> kjenteMeldingerAvansert = new List<string>()
-                {
-                    "no.ks.fiks.gi.arkivintegrasjon.oppdatering.arkivmelding.v1",
-                    "no.ks.fiks.gi.arkivintegrasjon.oppdatering.arkivmeldingUtgaaende.v1"
-                };
-
-            XmlSchemaSet arkivmeldingXmlSchemaSet = new XmlSchemaSet();
-
+            var arkivmeldingXmlSchemaSet = new XmlSchemaSet();
             arkivmeldingXmlSchemaSet.Add("http://www.arkivverket.no/standarder/noark5/arkivmelding", Path.Combine("schema", "arkivmelding.xsd"));
             arkivmeldingXmlSchemaSet.Add("http://www.arkivverket.no/standarder/noark5/metadatakatalog", Path.Combine("schema", "metadatakatalog.xsd"));
 
-            XmlSchemaSet sokXmlSchemaSet = new XmlSchemaSet();
+            var sokXmlSchemaSet = new XmlSchemaSet();
             sokXmlSchemaSet.Add("http://www.arkivverket.no/standarder/noark5/sok", Path.Combine("schema", "sok.xsd"));
 
-            bool xmlValidationErrorOccured = false;
+            var xmlValidationErrorOccured = false;
 
-            if (kjenteMeldingerBasis.Contains(mottatt.Melding.MeldingType))
+            if (MeldingTypeV1.IsBasis(mottatt.Melding.MeldingType))
             {
                 var validationResult = new List<List<string>>();
-                arkivmelding deserializedArkivmelding = new arkivmelding();
-                Console.WriteLine("Melding " + mottatt.Melding.MeldingId + " " + mottatt.Melding.MeldingType + " mottas...");
+                var deserializedArkivmelding = new arkivmelding();
+                Console.WriteLine($"Melding {mottatt.Melding.MeldingId} {mottatt.Melding.MeldingType} mottas...");
 
                 //TODO håndtere meldingen med ønsket funksjonalitet
                 if (mottatt.Melding.HasPayload)
@@ -125,7 +95,7 @@ namespace ks.fiks.io.arkivsystem.sample
                                         Console.WriteLine(text);
                                 }
                                 else
-                                    Console.WriteLine("Mottatt vedlegg: " + asiceReadEntry.FileName);
+                                    Console.WriteLine($"Mottatt vedlegg: {asiceReadEntry.FileName}");
 
                             }
                         }
@@ -147,22 +117,27 @@ namespace ks.fiks.io.arkivsystem.sample
                             feilmelding = "Feilmelding:\n" + string.Join("\n ", validationResult[0])
                         };
                         var errorMessage = mottatt.SvarSender.Svar(MeldingTypeV1.Ugyldigforespørsel, ugyldigforespørsel.ToString(), "ugyldigforespørsel.json").Result;
-                        
+                        Console.WriteLine($"Svarmelding {errorMessage.MeldingId} {errorMessage.MeldingType} sendt");
                         mottatt.SvarSender.Ack(); // Ack message to remove it from the queue
                     }
                     else
                     {
                         var svarmsg = mottatt.SvarSender.Svar(MeldingTypeV1.Mottatt).Result;
-                        Console.WriteLine("Svarmelding " + svarmsg.MeldingId + " " + svarmsg.MeldingType + " sendt...");
+                        Console.WriteLine($"Svarmelding {svarmsg.MeldingId} {svarmsg.MeldingType} sendt...");
 
                         Console.WriteLine("Melding er mottatt i arkiv ok ......");
 
                         mottatt.SvarSender.Ack(); // Ack message to remove it from the queue
                     }
                 }
-                else {
-                    var svarmsg = mottatt.SvarSender.Svar("no.ks.fiks.gi.arkivintegrasjon.feil.v1", "Meldingen mangler innhold", "feil.txt").Result;
-                    Console.WriteLine("Svarmelding " + svarmsg.MeldingId + " " + svarmsg.MeldingType + " Meldingen mangler innhold");
+                else { // Ugyldig forespørsel
+                    var ugyldigforespørsel = new Ugyldigforespørsel()
+                    {
+                        errorId = Guid.NewGuid().ToString(),
+                        feilmelding = "Meldingen mangler innhold"
+                    };
+                    var svarmsg = mottatt.SvarSender.Svar(MeldingTypeV1.Ugyldigforespørsel, ugyldigforespørsel.ToString(), "ugyldingforespørsel.json").Result;
+                    Console.WriteLine($"Svarmelding {svarmsg.MeldingId} {svarmsg.MeldingType} sendt");
 
                     mottatt.SvarSender.Ack(); // Ack message to remove it from the queue
 
@@ -201,12 +176,12 @@ namespace ks.fiks.io.arkivsystem.sample
                     string payload = Arkivintegrasjon.Serialize(kvittering);
 
                     var svarmsg2 = mottatt.SvarSender.Svar("no.ks.fiks.gi.arkivintegrasjon.kvittering.v1", payload, "arkivmelding.xml").Result;
-                    Console.WriteLine("Svarmelding " + svarmsg2.MeldingId + " " + svarmsg2.MeldingType + " sendt...");
+                    Console.WriteLine("$Svarmelding {svarmsg2.MeldingId} {svarmsg2.MeldingType} sendt...");
 
                     Console.WriteLine("Arkivering er ok ......");
                 }
             }
-            else if (kjenteMeldingerSok.Contains(mottatt.Melding.MeldingType))
+            else if (MeldingTypeV1.IsSok(mottatt.Melding.MeldingType))
             {
                 var validationResult = new List<List<string>>();
                 Console.WriteLine("Melding " + mottatt.Melding.MeldingId + " " + mottatt.Melding.MeldingType + " mottas...");
@@ -245,7 +220,13 @@ namespace ks.fiks.io.arkivsystem.sample
                     }
                     if (xmlValidationErrorOccured)
                     {
-                        var errorMessage = mottatt.SvarSender.Svar("no.ks.fiks.gi.arkivintegrasjon.feil.v1", String.Join("\n ", validationResult[0]), "feil.txt").Result;
+                        var ugyldigforespørsel = new Ugyldigforespørsel()
+                        {
+                            errorId = Guid.NewGuid().ToString(),
+                            feilmelding = "Feilmelding:\n" + string.Join("\n ", validationResult[0])
+                        };
+                        var errorMessage = mottatt.SvarSender.Svar(MeldingTypeV1.Ugyldigforespørsel, ugyldigforespørsel.ToString(), "ugyldigforespørsel.json").Result;
+                        Console.WriteLine($"Svarmelding {errorMessage.MeldingId} {errorMessage.MeldingType} sendt");
                         mottatt.SvarSender.Ack(); // Ack message to remove it from the queue
                     }
                 }
@@ -253,11 +234,10 @@ namespace ks.fiks.io.arkivsystem.sample
                 //Konverterer til arkivmelding xml
                 var simulertSokeresultat = MessageSamples.GetForenkletArkivmeldingInngåendeMedSaksreferanse();
                 var arkivmelding = Arkivintegrasjon.ConvertForenkletInnkommendeToArkivmelding(simulertSokeresultat);
-                string payload = Arkivintegrasjon.Serialize(arkivmelding);
+                var payload = Arkivintegrasjon.Serialize(arkivmelding);
                 //Lager FIKS IO melding
                 List<IPayload> payloads = new List<IPayload>();
                 payloads.Add(new StringPayload(payload, "arkivmelding.xml"));
-
 
                 var svarmsg = mottatt.SvarSender.Svar("no.ks.fiks.gi.arkivintegrasjon.innsyn.sok.resultat.v1", payloads).Result;
                 Console.WriteLine("Svarmelding " + svarmsg.MeldingId + " " + svarmsg.MeldingType + " sendt...");
@@ -265,10 +245,8 @@ namespace ks.fiks.io.arkivsystem.sample
                 Console.WriteLine("Melding er håndtert i arkiv ok ......");
 
                 mottatt.SvarSender.Ack(); // Ack message to remove it from the queue
-
-
             }
-            else if (kjenteMeldingerAvansert.Contains(mottatt.Melding.MeldingType))
+            else if (MeldingTypeV1.IsAvansert(mottatt.Melding.MeldingType))
             {
                 Console.WriteLine("Melding " + mottatt.Melding.MeldingId + " " + mottatt.Melding.MeldingType + " mottas...");
 
@@ -277,76 +255,12 @@ namespace ks.fiks.io.arkivsystem.sample
                 Console.WriteLine("Melding er håndtert i arkiv ok ......");
 
                 mottatt.SvarSender.Ack(); // Ack message to remove it from the queue
-
             }
             else
             {
                 Console.WriteLine("Ukjent melding i køen som avvises " + mottatt.Melding.MeldingId + " " + mottatt.Melding.MeldingType);
                 mottatt.SvarSender.Nack(); // Nack message to remove it from the queue
-
-
             }
-        }
-
-        private FiksIOClient CreateFiksIoClient()
-        {
-            Console.WriteLine("Setter opp FIKS integrasjon for arkivsystem...");
-            var accountId = appSettings.FiksIOConfig.FiksIoAccountId;
-            var privateKey = File.ReadAllText(appSettings.FiksIOConfig.FiksIoPrivateKey);
-            var integrationId = appSettings.FiksIOConfig.FiksIoIntegrationId; 
-            var integrationPassword = appSettings.FiksIOConfig.FiksIoIntegrationPassword;
-            var scope = appSettings.FiksIOConfig.FiksIoIntegrationScope;
-            var audience = appSettings.FiksIOConfig.MaskinPortenAudienceUrl;
-            var tokenEndpoint = appSettings.FiksIOConfig.MaskinPortenTokenUrl;
-            var issuer = appSettings.FiksIOConfig.MaskinPortenIssuer;
-            
-            var ignoreSSLError = Environment.GetEnvironmentVariable("AMQP_IGNORE_SSL_ERROR");
-
-
-            // Fiks IO account configuration
-            var account = new KontoConfiguration(
-                                accountId,
-                                privateKey);
-
-            // Id and password for integration associated to the Fiks IO account.
-            var integration = new IntegrasjonConfiguration(
-                                    integrationId,
-                                    integrationPassword, scope);
-
-            // ID-porten machine to machine configuration
-            var maskinporten = new MaskinportenClientConfiguration(
-                audience: audience,
-                tokenEndpoint: tokenEndpoint,
-                issuer: issuer,
-                numberOfSecondsLeftBeforeExpire: 10,
-                certificate: GetCertificate(appSettings));
-
-            // Optional: Use custom api host (i.e. for connecting to test api)
-            var api = new ApiConfiguration(
-                scheme: appSettings.FiksIOConfig.ApiScheme,
-                host: appSettings.FiksIOConfig.ApiHost,
-                port: appSettings.FiksIOConfig.ApiPort);
-            
-            var sslOption1 = (!string.IsNullOrEmpty(ignoreSSLError) && ignoreSSLError == "true")
-                ? new SslOption()
-                {
-                    Enabled = true,
-                    ServerName = appSettings.FiksIOConfig.AmqpHost,
-                    CertificateValidationCallback =
-                        (RemoteCertificateValidationCallback) ((sender, certificate, chain, errors) => true)
-                }
-                : null;
-                
-
-            // Optional: Use custom amqp host (i.e. for connection to test queue)
-            var amqp = new AmqpConfiguration(
-                host: appSettings.FiksIOConfig.AmqpHost, //"io.fiks.test.ks.no",
-                port: appSettings.FiksIOConfig.AmqpPort,
-                sslOption1);
-
-            // Combine all configurations
-            var configuration = new FiksIOConfiguration(account, integration, maskinporten, api, amqp);
-            return new FiksIOClient(configuration);
         }
 
         private void SubscribeToFiksIOClient()
@@ -364,42 +278,6 @@ namespace ks.fiks.io.arkivsystem.sample
             Console.WriteLine("Arkiv Service is stopping.");
 
             return Task.CompletedTask;
-        }
-
-        private static X509Certificate2 GetCertificate(string ThumbprintIdPortenVirksomhetssertifikat)
-        {
-
-            //Det samme virksomhetssertifikat som er registrert hos ID-porten
-            X509Store store = new X509Store(StoreLocation.CurrentUser);
-            X509Certificate2 cer = null;
-            store.Open(OpenFlags.ReadOnly);
-            //Henter Arkitektum sitt virksomhetssertifikat
-            X509Certificate2Collection cers = store.Certificates.Find(X509FindType.FindByThumbprint, ThumbprintIdPortenVirksomhetssertifikat, false);
-            if (cers.Count > 0)
-            {
-                cer = cers[0];
-            };
-            store.Close();
-
-            return cer;
-        }
-        
-        private static X509Certificate2 GetCertificate(AppSettings appSettings)
-        {
-            if (!string.IsNullOrEmpty(appSettings.FiksIOConfig.MaskinPortenCompanyCertificatePath))
-            {
-                return new X509Certificate2(File.ReadAllBytes(appSettings.FiksIOConfig.MaskinPortenCompanyCertificatePath), appSettings.FiksIOConfig.MaskinPortenCompanyCertificatePassword);
-            }
-           
-            var store = new X509Store(StoreLocation.CurrentUser);
-
-            store.Open(OpenFlags.ReadOnly);
-
-            var certificates = store.Certificates.Find(X509FindType.FindByThumbprint, appSettings.FiksIOConfig.MaskinPortenCompanyCertificateThumbprint, false);
-
-            store.Close();
-
-            return certificates.Count > 0 ? certificates[0] : null;
         }
     }
 }
